3e18942008d2abe3ebe349f3353bd766248007d7
Ben Cipollini <bcipolli@ucsd.edu>
2015-12-06 07:30:45 -0800

    BF: FFT stats must be computed in all cases, otherwise FFT plots choke.

--- code/analyze/ac/stats/de_StatsFFTs_TTest.m
+++ code/analyze/ac/stats/de_StatsFFTs_TTest.m
@@ -7,7 +7,7 @@ function [pals] = de_StatsFFTs(stats)
     % Now that we've smoothed each model instance's
     %   power spectrum according to the current Gaussian kernel,
     %   we need to do a statistical test at each frequency.
-
+    %
     nSig    = size(stats.model.power1D.mean{1},1);
     nModels = min( size(stats.model.power1D.mean{1},2), size(stats.model.power1D.mean{end},2) ); %match sample sizes
 
